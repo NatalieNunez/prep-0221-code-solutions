a2088233873dc3d4a97cb433b3ca06c05cb19f1c
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-02-24 15:45:26 -0800

    modified

---
+++ prep-javascript-functions/main.js
@@ -0,0 +1,19 @@
+function addTwoNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+console.log(addTwoNumbers(4, 4));
+
+function convertHoursToMinutes(number) {
+  return number * 60;
+}
+
+console.log(convertHoursToMinutes(2));
+
+function personalizeGreeting(name) {
+  return `Hello ${name}!`;
+}
+
+console.log(personalizeGreeting('Tim D'));
+
+
