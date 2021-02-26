af3321c29e555987eaacd9002079478fab1f4dc5
Natalie Nunez <natalie.nunez17@yahoo.com>
2021-02-25 18:24:13 -0800

    Completed prep-javascript-arrays exercise.

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,17 @@
+var languagesArray = ['html', 'css', 'javascript', 'react', 'node.js'];
+console.log('languages array:', languagesArray);
+console.log('array length:', languagesArray.length);
+languagesArray.push('C#', 'php');
+console.log('updated array:', languagesArray);
+languagesArray.pop();
+console.log('Last index removed:', languagesArray);
+languagesArray.unshift('C++');
+console.log('Added element to front:', languagesArray);
+languagesArray.shift();
+console.log('Removed first element:', languagesArray);
+var thirdElement = languagesArray[2];
+console.log('3rd element in array:', thirdElement);
+var arrayLength = languagesArray.length;
+console.log('The final length of the array is', arrayLength);
+var lastItem = languagesArray[arrayLength - 1];
+console.log('The last item in the array is', lastItem);
